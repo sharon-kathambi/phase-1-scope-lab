9c8bd3fde565710a41f4728f2709804b19d8baff
Sharon Mwenda <smkathambi@gmail.com>
2022-06-13 13:08:23 +0300

    commit tree c008ec48a1f9bf58c5df90428eb4906ea7d32f0d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob'
+// console.log(customerName);
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+};
+// console.log(upperCaseCustomerName(customerName));
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
+const leastFavoriteCustomer = 'Chris';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Jack';
+}
+
